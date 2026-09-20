8216b838987748457faff5377203985564906f72
Ridhimaa7 <158288605+Ridhimaa7@users.noreply.github.com>
2026-09-20 17:32:58 +0530

    Add: longest-increasing-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        cache = {}
+        def helper(i, prev_idx):
+            if i >= len(nums):
+                return 0
+            if (i, prev_idx) in cache:
+                return cache[(i, prev_idx)]
+            
+            # Skip current element
+            res = helper(i + 1, prev_idx)
+            
+            # Take current element if strictly greater than previous
+            if prev_idx == -1 or nums[i] > nums[prev_idx]:
+                res = max(res, 1 + helper(i + 1, i))
+                
+            cache[(i, prev_idx)] = res
+            return res
+            
+        return helper(0, -1)
